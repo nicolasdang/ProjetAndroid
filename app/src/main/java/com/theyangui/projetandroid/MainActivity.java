01fbc768f08b01eca8562177ca1601398e0e3916
Kelian Hutin <kelian.hutin@gmail.com>
2018-02-05 16:09:18 +0100

    up

--- app/src/main/java/com/theyangui/projetandroid/MainActivity.java
+++ app/src/main/java/com/theyangui/projetandroid/MainActivity.java
@@ -1,9 +1,12 @@
 package com.theyangui.projetandroid;
 
 import android.Manifest;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +14,8 @@ import android.view.View;
 public class MainActivity extends AppCompatActivity {
 
     private int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION;
+    NotificationCompat.Builder notification;
+    private static final int uniqueID = 45612;
 
 
     @Override
@@ -24,10 +29,26 @@ public class MainActivity extends AppCompatActivity {
                     MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
             return;
         }
+
+        notification = new NotificationCompat.Builder(this);
+        notification.setAutoCancel(true);
     }
 
     public void onClickHamButton(View view) {
         startActivity(new Intent(this, MapsActivity.class));
+
+        notification.setSmallIcon(R.drawable.ic_stat_name);
+        notification.setTicker("Hungry Now is running.");
+        notification.setWhen(System.currentTimeMillis());
+        notification.setContentTitle("Hungry Now");
+        notification.setContentText("You launched Hungry Now");
+
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        notification.setContentIntent(pendingIntent);
+
+        NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        nm.notify(uniqueID, notification.build());
     }
 
 
@@ -44,4 +65,5 @@ public class MainActivity extends AppCompatActivity {
 
             }
     }
+
 }
